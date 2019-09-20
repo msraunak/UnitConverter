d33049290db71f9d65f68878b798ab7b45ebc85f
msraunak <raunak@loyola.edu>
2019-09-20 13:26:16 -0400

    Initial commit

---
+++ app/src/main/java/edu/loyola/raunak/unitconverter/MainActivity.java
@@ -0,0 +1,80 @@
+package edu.loyola.raunak.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TempConverter tempConvModel;
+    public static String MA="MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tempConvModel = new TempConverter();
+    }
+
+    /**
+     * Called on the FtoC button is clicked
+     * @param view the current view
+     */
+    public void onClickFtoC(View view){
+
+        EditText etFarh = ( EditText )findViewById( R.id.editTextFarh);
+        double newFarhVal;
+        try {
+            newFarhVal = Double.parseDouble( etFarh.getText().toString() );
+            tempConvModel.setTempInF( newFarhVal );
+            tempConvModel.updateCelcius();
+
+            EditText etCelc = (EditText) findViewById (R.id.editTextCelc);
+            etCelc.setText( tempConvModel.getFormattedTempInC() );
+        } catch (NumberFormatException nfe){
+            Log.w(MA, "Non numeric value in farhenheight text ");
+        }
+    }
+
+    /**
+     * Called on the Celc to Farh button is clicked
+     * @param view the current view
+     */
+    public void onClickCtoF(View view){
+
+        EditText etCelc = ( EditText )findViewById( R.id.editTextCelc);
+        double newCelcVal;
+        try {
+            newCelcVal = Double.parseDouble( etCelc.getText().toString() );
+            tempConvModel.setTempInC( newCelcVal );
+            tempConvModel.updateFarhenheight();
+
+            EditText etFarh = (EditText) findViewById (R.id.editTextFarh);
+            etFarh.setText( tempConvModel.getFormattedTempInF() );
+        } catch (NumberFormatException nfe){
+            Log.w(MA, "Non numeric value in celcius text ");
+        }
+    }
+
+    /**
+     * When the reset button is clicked in the MainActivity Window
+     * @param view the current View
+     */
+    public void onClickReset(View view){
+        tempConvModel.reset();
+        ((EditText) findViewById( R.id.editTextFarh)).setText( tempConvModel.getFormattedTempInF() );
+        ((EditText) findViewById( R.id.editTextCelc)).setText( tempConvModel.getFormattedTempInC() );
+    }
+
+    /**
+     * When the exit app button is clicked
+     * @param view
+     */
+    public void onClickExit(View view) {
+        finishAffinity();
+        finishAndRemoveTask();
+    }
+}
---
+++ app/src/main/java/edu/loyola/raunak/unitconverter/TempConverter.java
@@ -0,0 +1,81 @@
+package edu.loyola.raunak.unitconverter;
+
+import java.text.DecimalFormat;
+
+/**
+ * Model for Temperature unit conversion
+ * @author M S Raunak
+ * @author Sibren Isaacman
+ * @version 1.0 8/29/2019
+ */
+public class TempConverter {
+
+    /** Temperature in Celcius*/
+    private double tempInC;
+    /** Temperature in Farhenheight */
+    private double tempInF;
+
+    private DecimalFormat df;
+
+    /**
+     * Default Constructor
+     */
+    public TempConverter (){
+        this.tempInC = 0.0;
+        this.tempInF = 32.0;
+        df = new DecimalFormat ("#.##"); // Format values up to two decimal places
+    }
+
+    /**
+     * parameterized constructur
+     * @param celc the temperature value in Celcius
+     * @param farh the temperature value in Farhenheight
+     */
+    public TempConverter (double celc, double farh){
+        this.tempInC = celc;
+        this.tempInF = farh;
+        df = new DecimalFormat ( "#.##");
+    }
+
+
+    public double getTempInC() {
+        return tempInC;
+    }
+
+    public String getFormattedTempInC(){
+        return df.format( tempInC );
+    }
+
+    public void setTempInC(double newTempInC) {
+        this.tempInC = newTempInC;
+    }
+
+    public double getTempInF() {
+        return tempInF;
+    }
+
+    public String getFormattedTempInF() {
+        return df.format(tempInF);
+    }
+
+    public void setTempInF(double newTempInF) {
+        this.tempInF = newTempInF;
+    }
+
+    /** Update the Celcius value based on the current Farhenheight value */
+    public void updateCelcius(){
+        tempInC = ( (tempInF - 32)/9 ) * 5;
+    }
+
+    /** Update the Farhenheight value based on the current Celcius value */
+    public void updateFarhenheight(){
+        tempInF = ( (tempInC / 5.0 ) * 9.0 ) + 32;
+    }
+
+    public void reset(){
+        tempInF = 32.0;
+        tempInC = 0.0;
+    }
+
+
+}
